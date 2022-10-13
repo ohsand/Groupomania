8174d00798d14d8554ede229e9434571d178db29
ohsand <osand.business@gmail.com>
2022-10-13 21:18:22 +0200

    add password hash

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../config/db')
+const bcrypt = require("bcrypt")
+
 
 router.post("/register", (req, res) => {
 
     const username = req.body.username;
     const password = req.body.password;
-
-    db.query(
-        "INSERT INTO users (username, password) VALUES (?, ?);", [username, password],
-        (err, results) => {
-        console.log(err);
-        res.send(results);
-    }
-    );
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, function(err, hashPassword) {
+            // Store hash in the database
+            db.query(
+                "INSERT INTO users (username, password) VALUES (?, ?);", [username, hashPassword],
+                (err, results) => {
+                console.log(err);
+                res.send(results);
+                console.log("acc created");
+            }
+            );
+        });
+    })
 });
 
 router.post("/login", (req, res) => {
@@ -29,12 +36,15 @@ router.post("/login", (req, res) => {
                 console.log(err)
             }
             if (results.length > 0) {
-                if (password == results[0].password) {
+                  bcrypt.compare(password, results[0].password, function(err, validPassword) {
+                    if (validPassword) {
+                       // password is valid
                     console.log("You are logged in!");
                     res.json({loggedIn: true, username: username});
                 } else {
                     res.json({loggedIn: false, message: "Wrong username, password combination"});
                 }
+                });
             } else {
                 res.json({loggedIn: false, message: "This user does not exist"});
             }
